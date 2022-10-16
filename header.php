98f6bc2a280ceb1ec21a4a7e8a408c4ccd4da466
RioJavier15 <rioreyhanjavier@gmail.com>
2022-10-16 21:09:24 +0700

    login dan home

---
+++ header.php
@@ -0,0 +1,135 @@
+<!-- <?php $page = substr($_SERVER['SCRIPT_NAME'],strrpos($_SERVER['SCRIPT_NAME'],"/")+1); ?> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sidebar Menu</title>
+  <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="css/home.css">
+</head>
+
+<body>
+  <nav>
+    <div class="sidebar-top">
+      <span class="shrink-btn">
+        <i class='bx bx-chevron-left'></i>
+      </span>
+      <img src="./img/logo.png" class="logo" alt="">
+      <h3 class="hide">INFO INFO</h3>
+    </div>
+
+    <div class="search">
+      <i class='bx bx-search'></i>
+      <input type="text" class="hide" placeholder="Quick Search ...">
+    </div>
+
+    <div class="sidebar-links">
+      <ul>
+        <div class="active-tab"></div>
+        <li class="tooltip-element" data-tooltip="0">
+          <a href="home.php" class="active" data-active="0">
+            <div class="icon">
+              <i class='bx bx-cart'></i>
+              <i class='bx bxs-cart'></i>
+            </div>
+            <span class="link hide">Transaksi</span>
+          </a>
+        </li>
+        <li class="tooltip-element" data-tooltip="1">
+          <a href="produk.php" data-active="1">
+            <div class="icon">
+              <i class='bx bx-box'></i>
+              <i class='bx bxs-box'></i>
+            </div>
+            <span class="link hide">Produk</span>
+          </a>
+        </li>
+        <li class="tooltip-element" data-tooltip="2">
+          <a href="#" data-active="2">
+            <div class="icon">
+              <i class='bx bx-user-check'></i>
+              <i class='bx bxs-user-check'></i>
+            </div>
+            <span class="link hide">Pelanggan</span>
+          </a>
+        </li>
+        <li class="tooltip-element" data-tooltip="3">
+          <a href="#" data-active="3">
+            <div class="icon">
+              <i class='bx bx-bar-chart-square'></i>
+              <i class='bx bxs-bar-chart-square'></i>
+            </div>
+            <span class="link hide">Riwayat</span>
+          </a>
+        </li>
+        <div class="tooltip">
+          <span class="show">Transaksi</span>
+          <span>Produk</span>
+          <span>Pelanggan</span>
+          <span>Riwayat</span>
+        </div>
+      </ul>
+
+      <!-- <h4 class="hide">Shortcuts</h4>
+
+      <ul>
+        <li class="tooltip-element" data-tooltip="0">
+          <a href="#" data-active="4">
+            <div class="icon">
+              <i class='bx bx-notepad'></i>
+              <i class='bx bxs-notepad'></i>
+            </div>
+            <span class="link hide">Tasks</span>
+          </a>
+        </li>
+        <li class="tooltip-element" data-tooltip="1">
+          <a href="#" data-active="5">
+            <div class="icon">
+              <i class='bx bx-help-circle'></i>
+              <i class='bx bxs-help-circle'></i>
+            </div>
+            <span class="link hide">Help</span>
+          </a>
+        </li>
+        <li class="tooltip-element" data-tooltip="2">
+          <a href="#" data-active="6">
+            <div class="icon">
+              <i class='bx bx-cog'></i>
+              <i class='bx bxs-cog'></i>
+            </div>
+            <span class="link hide">Settings</span>
+          </a>
+        </li>
+        <div class="tooltip">
+          <span class="show">Tasks</span>
+          <span>Help</span>
+          <span>Settings</span>
+        </div>
+      </ul>
+    </div> -->
+
+    <div class="sidebar-footer">
+      <a href="#" class="account tooltip-element" data-tooltip="0">
+        <i class='bx bx-user'></i>
+      </a>
+      <div class="admin-user tooltip-element" data-tooltip="1">
+        <div class="admin-profile hide">
+          <img src="./img/face-1.png" alt="">
+          <div class="admin-info">
+            <h3>Rio</h3>
+            <h5>Admin</h5>
+          </div>
+        </div>
+        <a href="index.html" class="log-out">
+          <i class='bx bx-log-out'></i>
+        </a>
+      </div>
+      <div class="tooltip">
+        <span class="show">Rio Javier</span>
+        <span>Logout</span>
+      </div>
+    </div>
+  </nav>
---
+++ home.php
@@ -0,0 +1,58 @@
+<?php 
+include 'header.php';
+?>
+
+
+  <main>
+    <h1>My Dashboard</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Kode Produk</th>
+          <th>Nama</th>
+          <th>Harga</th>
+          <th>Stok</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>20 mbps</td>
+          <td>250000</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>20 mbps</td>
+          <td>250000</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>20 mbps</td>
+          <td>250000</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>20 mbps</td>
+          <td>250000</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>20 mbps</td>
+          <td>250000</td>
+          <td>100</td>
+        </tr>
+      </tbody>
+    </table>
+    <p class="copyright">
+      &copy; 2021 - <span>Aqumex</span> All Rights Reserved.
+    </p>
+  </main>
+  <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+  <script src="js/home.js"></script>
+</body>
+
+</html>
