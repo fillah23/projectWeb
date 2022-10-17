7b57512afb43b6784a12612ad1c8d174b0453ddc
fillah23 <fillahseptian7l24@gmail.com>
2022-10-17 16:22:25 +0700

    tabel

--- pages/produk.php
+++ pages/produk.php
@@ -1,47 +1,188 @@
 <main>
-    <h1>Produk</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Kode Produk</th>
-                <th>Nama</th>
-                <th>Harga</th>
-                <th>Stok</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>20 mbps</td>
-                <td>250000</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>20 mbps</td>
-                <td>250000</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>20 mbps</td>
-                <td>250000</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>20 mbps</td>
-                <td>250000</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>20 mbps</td>
-                <td>250000</td>
-                <td>100</td>
-            </tr>
-        </tbody>
-    </table>
+    
+    <!-- table -->
+    <div class="details">
+        <div class="recentOrders">
+            <div class="cardHeader">
+                <h2>
+                    Produk
+                </h2>
+                <a href="#" class="btn">Tambah</a>
+            </div>
+            <div class="table-wrapper">
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            Kode Produk
+                        </td>
+                        <td>
+                            Nama
+                        </td>
+                        <td>
+                            Harga
+                        </td>
+                        <td>
+                            Stok
+                        </td>
+                        <td>
+                            Aksi
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            20Mbps
+                        </td>
+                        <td>
+                            150.000
+                        </td>
+                        <td>
+                            50
+                        </td>
+                        <td>
+                            <span class="status delivered">Ready
+                            </span>
+                        </td>
+                    </tr>
+                    
+                    
+                </tbody>
+            </table>
+            </div>
+            
+        </div>
+    </div>
     <div class="footer">
         <p class="copyright">
             &copy; 2021 - <span>Aqumex</span> All Rights Reserved.
