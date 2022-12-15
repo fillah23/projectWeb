88e1de896dca9b08f89e3f36b38b0055ba287b5c
faryna03 <nabilazhafarina.nz@gmail.com>
2022-12-15 13:40:27 +0700

    new API faq&transaksi

--- API/transaksi.php
+++ API/transaksi.php
@@ -1,8 +1,9 @@
 <?php
 $connect = new mysqli("localhost", "root", "", "fans");
+$kode = $_POST["kode_pelanggan"];
 $queryResult=$connect->query("SELECT * FROM pelanggan join produk on pelanggan.kode_produk = 
 produk.kode_produk join transaksi on pelanggan.kode_pelanggan = transaksi.kode_pelanggan 
-join akun on transaksi.kode_akun = akun.kode_akun");
+join akun on transaksi.kode_akun = akun.kode_akun where pelanggan.kode_pelanggan ='".$kode."'");
 $result=array();
 while($fetchData=$queryResult->fetch_assoc()){
     $result[]=$fetchData;
